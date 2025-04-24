6b0c948411a9159489789f44c47a9b975e1d3bd0
dawn <1194815021@qq.com>
2025-04-24 10:18:13 +0800

    refactor: initialize and register the global component

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { initAppConfigStore } from '@/logics/initAppConfig';
 import { router, setupRouter } from '@/router';
 import { setupRouterGuard } from '@/router/guard';
 import { setupStore } from '@/stores';
-// import { registerGlobComp } from '@/components/registerGlobComp';
+import { registerGlobComp } from '@/components/registerGlobComp';
 async function bootstrap() {
   const app = createApp(App);
 
@@ -17,6 +17,10 @@ async function bootstrap() {
   // Initialize internal system configuration
   // 初始化内部系统配置
   initAppConfigStore();
+
+  // Register global components
+  // 注册全局组件
+  registerGlobComp(app);
 
   // Configure routing
   // 配置路由
