54b24e5d3a7013b044b944c0d97ab516b034e6c6
dawn <1194815021@qq.com>
2025-04-25 10:08:35 +0800

    refactor: basic layout

---
+++ src/layouts/default/sider/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>sider</div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, ref } from 'vue';
+  export default defineComponent({
+    name: 'SiderWrapper',
+    setup() {
+      return {};
+    },
+  });
+</script>
