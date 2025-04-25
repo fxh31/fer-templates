4ed7b8c547f78bd41a2454e1e070de92e2ee5857
dawn <1194815021@qq.com>
2025-04-25 10:08:35 +0800

    refactor: basic layout

---
+++ src/layouts/default/footer/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>footer</div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, ref } from 'vue';
+  export default defineComponent({
+    name: 'LayoutFooter',
+    setup() {
+      return {};
+    },
+  });
+</script>
