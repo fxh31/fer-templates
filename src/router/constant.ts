fec8d7e54f1dfff6950de73f146894f713b734f5
dawn <1194815021@qq.com>
2025-04-25 15:57:31 +0800

    feat: 404 page and route

--- src/router/constant.ts
+++ src/router/constant.ts
@@ -1,3 +1,9 @@
+export const PAGE_NOT_FOUND_NAME = 'PageNotFound';
+
+/**
+ * @description: page not found（404）
+ */
+export const EXCEPTION_COMPONENT = () => import('@/views/basic/exception/Exception.vue');
 /**
  * @description: default layout
  */
