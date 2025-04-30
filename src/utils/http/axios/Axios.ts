021e229a99c4bd97c90631de3049661dfd220727
dawn <1194815021@qq.com>
2025-04-30 17:42:27 +0800

    fix: remove console and debugger in Axios.ts

--- src/utils/http/axios/Axios.ts
+++ src/utils/http/axios/Axios.ts
@@ -108,8 +108,6 @@ export class VAxios {
       this.axiosInstance
         .request<any, AxiosResponse<Result>>(conf)
         .then((res: AxiosResponse<Result>) => {
-          console.log(conf);
-          debugger;
           if (transformResponseHook && isFunction(transformResponseHook)) {
             try {
               const ret = transformResponseHook(res, opt);
