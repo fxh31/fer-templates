d2fdec85f3dadef66d24b22e0cb13cfc7766a855
dawn <1194815021@qq.com>
2025-04-30 17:49:28 +0800

    fix: declare type ElRef

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
+declare interface Fn<T = any, R = T> {
+  (...arg: T[]): R;
+}
+
 declare type ElRef<T extends HTMLElement = HTMLDivElement> = Nullable<T>;
-declare interface Fn<T = any, R = T> {
-  (...arg: T[]): R;
-}
-=======
-declare interface Fn<T = any, R = T> {
-  (...arg: T[]): R;
-}
->>>>>>> 7b48236 (feat: create async components)
