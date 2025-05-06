f42b5fc27e2041da724e35d645c556f7014c644d
dawn <1194815021@qq.com>
2025-05-06 17:45:40 +0800

    feat: design global css variable (less)

---
+++ build/generate/generateModifyVars.ts
@@ -0,0 +1,17 @@
+import { resolve } from 'path';
+
+/**
+ * less global variable
+ */
+export function generateModifyVars() {
+  return {
+    // hack: `true;@import (reference) './src/design/config.less'`,
+    hack: `true; @import (reference) "${resolve('src/design/config.less')}";`,
+    'info-color': 'blue',
+    'primary-color': 'blue',
+    'success-color': '#55D187', //  Success color
+    'error-color': '#ED6F6F', //  False color
+    'warning-color': '#EFBD47', //   Warning color
+    'btn-info-color': '#909399',
+  };
+}
