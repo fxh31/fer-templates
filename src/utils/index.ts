c064d73546bb9e8191ec268aa8a6a4f32caa5a87
dawn <1194815021@qq.com>
2025-05-06 11:31:15 +0800

    feat: withInstall

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import type { App, Component } from 'vue';
+
 import { cloneDeep, isEqual, mergeWith, unionWith } from 'lodash-es';
 import { isArray, isObject } from '@/utils/is';
 
@@ -38,3 +40,31 @@ export function deepMerge<T extends object | null | undefined, U extends object 
     }
   });
 }
+
+/**
+ * vue component global registration
+ */
+// https://github.com/vant-ui/vant/issues/8302
+type EventShim = {
+  new (...args: any[]): {
+    $props: {
+      onClick?: (...args: any[]) => void;
+    };
+  };
+};
+export type WithInstall<T> = T & {
+  install(app: App): void;
+} & EventShim;
+
+export type CustomComponent = Component & { displayName?: string };
+export const withInstall = <T extends CustomComponent>(component: T, alias?: string) => {
+  component.install = (app: App) => {
+    const compName = component.name || component.displayName;
+    if (!compName) return;
+    app.component(compName, component);
+    if (alias) {
+      app.config.globalProperties[alias] = component;
+    }
+  };
+  return component as WithInstall<T>;
+};
