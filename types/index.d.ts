e0dec312cb8cc7ead6af96b745e3dfe7dcd4b260
dawn <1194815021@qq.com>
2025-05-15 17:13:05 +0800

    feat(ts): declare global type

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,9 @@
 declare interface Fn<T = any, R = T> {
   (...arg: T[]): R;
 }
+
+declare type RefType<T> = T | null;
+
+declare type ComponentRef<T extends HTMLElement = HTMLDivElement> = ComponentElRef<T> | null;
+
+declare type ElRef<T extends HTMLElement = HTMLDivElement> = Nullable<T>;
