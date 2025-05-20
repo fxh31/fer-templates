45e502401ecca89d6012b14de57e05663f5a0dc2
dawn <1194815021@qq.com>
2025-05-20 11:06:52 +0800

    feat: jsx

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -1,12 +1,13 @@
 import type { PluginOption } from 'vite';
 
 import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 import { configThemePlugin } from './theme';
 
 // 配置插件（区分生产环境和开发环境）
 export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
-  const vitePlugins: (PluginOption | PluginOption[])[] = [vue()];
+  const vitePlugins: (PluginOption | PluginOption[])[] = [vue(), vueJsx()];
 
   // vite-plugin-theme
   vitePlugins.push(configThemePlugin(isBuild));
