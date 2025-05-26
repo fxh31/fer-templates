a0cac34c6d4f0f588288a58cd0fedda89f0c172f
dawn <1194815021@qq.com>
2025-05-26 15:29:08 +0800

    feat(router): white routes; base route; setRouter

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,10 +3,25 @@ import type { App } from 'vue';
 
 import { createRouter, createWebHistory } from 'vue-router';
 import { basicRoutes } from './routes';
+import { getferfAppId } from '@/utils/fer';
+
+// 路由白名单（白名单应该包含基本静态路由）
+const WHITE_NAME_LIST: string[] = [];
+const getRouteNames = (array: any[]) =>
+  array.forEach(item => {
+    WHITE_NAME_LIST.push(item.name);
+    getRouteNames(item.children || []);
+  });
+getRouteNames(basicRoutes);
+
+// 取路径上的应用识别码的作为基础路由
+let VITE_PUBLIC_PATH = import.meta.env.VITE_PUBLIC_PATH;
+const appId = getJnpfAppId();
+if (appId) VITE_PUBLIC_PATH = '/' + appId;
 
 export const router = createRouter({
   // 创建一个 hash 历史记录。
-  history: createWebHistory(),
+  history: createWebHistory(VITE_PUBLIC_PATH),
   // 添加到路由的初始路由列表。
   routes: basicRoutes as unknown as RouteRecordRaw[],
   // 是否应该禁止尾部斜杠。默认为假
@@ -18,4 +33,15 @@ export const router = createRouter({
 // 配置路由器
 export function setupRouter(app: App<Element>) {
   app.use(router);
+}
+
+// reset router
+// 重置路由
+export function resetRouter() {
+  router.getRoutes().forEach(route => {
+    const { name } = route;
+    if (name && !WHITE_NAME_LIST.includes(name as string)) {
+      router.hasRoute(name) && router.removeRoute(name);
+    }
+  });
 }
