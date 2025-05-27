f739409baa4749a55fdf64921178f59ed05fceaf
dawn <1194815021@qq.com>
2025-05-27 10:42:07 +0800

    feat: useBreakpoint for monitor screen

--- src/components/Application/src/AppProvider.vue
+++ src/components/Application/src/AppProvider.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { defineComponent, ref, toRefs } from 'vue';
   import { createAppProviderContext } from './useAppContext';
+  import { createBreakpointListen } from '@/hooks/event/useBreakpoint';
   import { getSlot } from '@/utils/helper';
   import { prefixCls } from '@/settings/designSetting';
 
@@ -14,10 +15,24 @@
     props,
     setup(props, { slots }) {
       const isMobile = ref(false);
-      const { prefixCls } = toRefs(props);
 
+      // Monitor screen breakpoint information changes
+      createBreakpointListen(({ screenMap, sizeEnum, width }) => {
+        const lgWidth = screenMap.get(sizeEnum.LG);
+        if (lgWidth) {
+          isMobile.value = width.value - 1 < lgWidth;
+        }
+        handleRestoreState();
+      });
+
+      const { prefixCls } = toRefs(props);
       // Inject variables into the global
       createAppProviderContext({ prefixCls, isMobile });
+
+      // Used to maintain the state before the window changes
+      function handleRestoreState() {
+        // todo: 进行一些菜单样式的变化
+      }
 
       return () => getSlot(slots);
     },
