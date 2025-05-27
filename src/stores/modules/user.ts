a32c91b10f5a33d4237ca476399f75c9ae030a02
dawn <1194815021@qq.com>
2025-05-27 18:09:19 +0800

    feat: set and get permission list by user store

--- src/stores/modules/user.ts
+++ src/stores/modules/user.ts
@@ -1,16 +1,43 @@
-import { defineStore } from 'pinia';
+import type { UserInfo, PermissionInfo } from '#/store';
+import type { BackMenu } from '@/api/basic/model/userModel';
 
-export const useUserStore = defineStore({
-  id: 'app-user',
-  state: () => ({
+import { PERMISSIONS_KEY, TOKEN_KEY, USER_INFO_KEY } from '@/enums/cacheEnum';
+
+import { defineStore } from 'pinia';
+import { getAuthCache, setAuthCache } from '@/utils/auth';
+
+interface UserState {
+  userInfo: Nullable<UserInfo>;
+  token?: string;
+  permissionList: PermissionInfo[];
+  backMenuList: BackMenu[];
+  backRouterList: BackMenu[];
+  sessionTimeout?: boolean;
+  lastUpdateTime: number;
+}
+
+export const useUserStore = defineStore('app-user', {
+  state: (): UserState => ({
+    permissionList: [],
     backRouterList: [],
   }),
   getters: {
+    getPermissionList(): PermissionInfo[] {
+      return this.permissionList.length ? this.permissionList : getAuthCache<PermissionInfo[]>(PERMISSIONS_KEY);
+    },
     getBackRouterList(): string[] {
       return this.backRouterList;
     },
   },
-  actions: {},
+  actions: {
+    setPermissionList(permissionList: PermissionInfo[]) {
+      this.permissionList = permissionList;
+      setAuthCache(PERMISSIONS_KEY, permissionList);
+    },
+    setBackMenuList(backMenuList: BackMenu[]) {
+      this.backMenuList = backMenuList;
+    },
+  },
 });
 
 // Need to be used outside the setup（todo..）
