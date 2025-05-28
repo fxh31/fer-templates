e164abc03b3f8aced3d1df23f106a5cb5f971b0b
dawn <1194815021@qq.com>
2025-05-28 15:33:04 +0800

    refactor(fer): input and inputTextArea

---
+++ src/components/Fer/Input/index.ts
@@ -0,0 +1,6 @@
+import { withInstall } from '@/utils';
+import Input from './src/Input.vue';
+import Textarea from './src/Textarea.vue';
+
+export const FerInput = withInstall(Input);
+export const FerTextarea = withInstall(Textarea);
---
+++ src/components/Fer/index.ts
@@ -0,0 +1,6 @@
+// fer 组件
+import { FerInput, FerTextarea } from '@/components/Fer/Input';
+import { FerAlert } from '@/components/Fer/Alert';
+import { FerDivider } from '@/components/Fer/Divider';
+
+export { FerAlert, FerInput, FerTextarea, FerDivider };
