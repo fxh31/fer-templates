744ab00fa4d4a407833ff218433ec4e1525c517b
dawn <1194815021@qq.com>
2025-05-28 15:31:06 +0800

    refactor(fer): divider

---
+++ src/components/Fer/Divider/src/Divider.vue
@@ -0,0 +1,23 @@
+<template>
+  <Divider :orientation="contentPosition" v-bind="getBindValue">{{ content }}</Divider>
+</template>
+
+<script lang="ts" setup>
+  import { Divider } from 'ant-design-vue';
+  import { computed, unref, useAttrs } from 'vue';
+
+  defineOptions({ name: 'ferDivider', inheritAttrs: false });
+  defineProps({
+    content: {
+      type: String,
+      default: '',
+    },
+    contentPosition: {
+      type: String as PropType<'left' | 'center' | 'right'>,
+      default: 'left',
+    },
+  });
+  const attrs = useAttrs({ excludeDefaultKeys: false });
+
+  const getBindValue = computed(() => ({ ...unref(attrs) }));
+</script>
