7aa36ed686a64f0eb592b4fbf17a6cf76b650edc
dawn <1194815021@qq.com>
2025-05-30 11:36:12 +0800

    build(icon): generate icon resources by iconify

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -2,12 +2,20 @@ import type { PluginOption } from 'vite';
 
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import purgeIcons from 'vite-plugin-purge-icons';
 
 import { configThemePlugin } from './theme';
+import { configSvgIconsPlugin } from './svgSprite';
 
 // 配置插件（区分生产环境和开发环境）
 export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
   const vitePlugins: (PluginOption | PluginOption[])[] = [vue(), vueJsx()];
+
+  // vite-plugin-svg-icons
+  vitePlugins.push(configSvgIconsPlugin(isBuild));
+
+  // vite-plugin-purge-icons
+  vitePlugins.push(purgeIcons());
 
   // vite-plugin-theme
   vitePlugins.push(configThemePlugin(isBuild));
