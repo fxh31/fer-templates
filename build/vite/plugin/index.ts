a35d72cd72f7de8cf025896506a53cf43302a5e1
dawn <1194815021@qq.com>
2025-06-03 10:18:21 +0800

    build(style): integration windi css

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -3,6 +3,7 @@ import type { PluginOption } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import purgeIcons from 'vite-plugin-purge-icons';
+import windiCSS from 'vite-plugin-windicss';
 
 import { configThemePlugin } from './theme';
 import { configSvgIconsPlugin } from './svgSprite';
@@ -10,6 +11,9 @@ import { configSvgIconsPlugin } from './svgSprite';
 // 配置插件（区分生产环境和开发环境）
 export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
   const vitePlugins: (PluginOption | PluginOption[])[] = [vue(), vueJsx()];
+
+  // vite-plugin-windicss
+  vitePlugins.push(windiCSS());
 
   // vite-plugin-svg-icons
   vitePlugins.push(configSvgIconsPlugin(isBuild));
