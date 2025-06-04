4a7697a9d5f44830e0158a312a2f14e4e0360837
dawn <1194815021@qq.com>
2025-06-04 11:15:20 +0800

    feat(directives): custome directive to handle click outside of element

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,6 +1,8 @@
 import type { App } from 'vue';
 
 import { setupLoadingDirective } from './loading';
+import { setupClickOutsideDirective } from './clickOutside';
 export function setupGlobDirectives(app: App) {
   setupLoadingDirective(app);
+  setupClickOutsideDirective(app);
 }
