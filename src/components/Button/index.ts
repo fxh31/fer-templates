da82109cc28ae3da3a0c12c76bcf765bc3232de0
dawn <1194815021@qq.com>
2025-06-04 14:34:50 +0800

    refactor(component): confirmButton and PopConfirmButton extend createConfirm and Popconfirm in Button

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -3,6 +3,10 @@ import type { ExtractPropTypes } from 'vue';
 import { withInstall } from '@/utils';
 import { buttonProps } from './src/props';
 import button from './src/BasicButton.vue';
+import popConfirmButton from './src/PopConfirmButton.vue';
+import modelConfirmButton from './src/ModelConfirmButton.vue';
 
 export declare type ButtonProps = Partial<ExtractPropTypes<typeof buttonProps>>;
 export const Button = withInstall(button);
+export const PopConfirmButton = withInstall(popConfirmButton);
+export const ModelConfirmButton = withInstall(modelConfirmButton);
