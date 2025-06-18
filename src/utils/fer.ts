e22d223526d002adb8a62a9f0fcf6d716c6386ca
dawn <1194815021@qq.com>
2025-06-18 18:30:50 +0800

    feat(utils): convert number for amount

--- src/utils/fer.ts
+++ src/utils/fer.ts
@@ -1,3 +1,67 @@
+/**
+ * 将数字金额转换为中文大写金额（适用于财务、发票等正式场景）
+ * Convert digital amounts to capitalized Chinese amounts (applicable to formal scenarios such as finance and invoices)
+ * @example 123456789 -> 壹亿贰仟叁佰肆拾伍万陆仟柒佰捌拾玖元整
+ */
+export function getAmountChinese(val) {
+  if (!val && val !== 0) return '';
+  if (val == 0) return '零元整';
+  const regExp = /[a-zA-Z]/;
+  if (regExp.test(val)) return '数字较大溢出';
+  let amount = +val;
+  if (isNaN(amount)) return '';
+  if (amount < 0) amount = Number(amount.toString().split('-')[1]);
+  const NUMBER = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖'];
+  const N_UNIT1 = ['', '拾', '佰', '仟'];
+  const N_UNIT2 = ['', '万', '亿', '兆'];
+  const D_UNIT = ['角', '分', '厘', '毫'];
+  let [integer, decimal] = amount.toString().split('.');
+  if (integer && (integer.length > 15 || integer.indexOf('e') > -1)) return '数字较大溢出';
+  let res = '';
+  // 整数部分
+  if (integer) {
+    let zeroCount = 0;
+    for (let i = 0, len = integer.length; i < len; i++) {
+      const num = integer.charAt(i);
+      const pos = len - i - 1; // 排除个位后 所处的索引位置
+      const q = pos / 4;
+      const m = pos % 4;
+      if (num === '0') {
+        zeroCount++;
+      } else {
+        if (zeroCount > 0 && m !== 3) res += NUMBER[0];
+        zeroCount = 0;
+        res += NUMBER[parseInt(num)] + N_UNIT1[m];
+      }
+      if (m == 0 && zeroCount < 4) res += N_UNIT2[Math.floor(q)];
+    }
+  }
+  if (Number(integer) != 0) res += '元';
+  // 小数部分
+  if (parseInt(decimal)) {
+    for (let i = 0; i < 4; i++) {
+      const num = decimal.charAt(i);
+      if (parseInt(num)) res += NUMBER[num] + D_UNIT[i];
+    }
+  } else {
+    res += '整';
+  }
+  if (val < 0) res = '负数' + res;
+  return res;
+}
+/**
+ * 转千位分隔
+ * Rotate the thousand-digit separation
+ * @example 123456789 -> '123,456,789'
+ */
+export function thousandsFormat(num) {
+  if (num === 0) return '0';
+  if (!num) return '';
+  const numArr = num.toString().split('.');
+  numArr[0] = numArr[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return numArr.join('.');
+}
+
 export function getDateFormat(format) {
   if (!format) return 'YYYY-MM-DD HH:mm:ss';
   const formatObj = {
