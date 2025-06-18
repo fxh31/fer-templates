ddef5b3e1d5879242e360e63b643e6d347d9e0b6
dawn <1194815021@qq.com>
2025-06-18 16:21:21 +0800

    test(component): basicTree

---
+++ src/components/Tree/demo/BasicLeftTree.vue
@@ -0,0 +1,25 @@
+<template>
+  <BasicLeftTree :treeData="treeData" defaultExpandAll title="88sss" search />
+</template>
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import { BasicLeftTree } from '..';
+
+  const treeData = ref([
+    {
+      id: 1,
+      fullName: 'test',
+
+      children: [
+        {
+          id: 2,
+          fullName: 'test2',
+        },
+      ],
+    },
+    {
+      id: 3,
+      fullName: 'bob1',
+    },
+  ]);
+</script>
---
+++ src/components/Tree/demo/BasicTree.vue
@@ -0,0 +1,25 @@
+<template>
+  <BasicTree :treeData="treeData" defaultExpandAll title="88" search />
+</template>
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import { BasicTree } from '../';
+
+  const treeData = ref([
+    {
+      id: 1,
+      fullName: 'test',
+
+      children: [
+        {
+          id: 2,
+          fullName: 'test2',
+        },
+      ],
+    },
+    {
+      id: 3,
+      fullName: 'bob1',
+    },
+  ]);
+</script>
