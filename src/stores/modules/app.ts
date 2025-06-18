e58369c68e8397aa06092c96b656723e4dd1d22a
dawn <1194815021@qq.com>
2025-06-18 18:28:27 +0800

    feat: complete the app store

--- src/stores/modules/app.ts
+++ src/stores/modules/app.ts
@@ -1,5 +1,14 @@
-import type { ProjectConfig } from '#/config';
+import type { ProjectConfig, HeaderSetting, MenuSetting, TransitionSetting, MultiTabsSetting } from '#/config';
+import type { BeforeMiniState, SysConfigInfo } from '#/store';
+
+import { defineStore } from 'pinia';
+import { store } from '@/stores';
 import { ThemeEnum } from '@/enums/appEnum';
+import { APP_DARK_MODE_KEY_, PROJ_CFG_KEY } from '@/enums/cacheEnum';
+import { Persistent } from '@/utils/cache/persistent';
+import { darkMode } from '@/settings/designSetting';
+import { resetRouter } from '@/router';
+import { deepMerge } from '@/utils';
 
 interface AppState {
   darkMode?: ThemeEnum;
@@ -9,4 +18,80 @@ interface AppState {
   projectConfig: ProjectConfig | null;
   // When the window shrinks, remember some states, and restore these states when the window is restored
   beforeMiniInfo: BeforeMiniState;
+}
+
+let timeId: TimeoutHandle;
+export const useAppStore = defineStore('app', {
+  state: (): AppState => ({
+    darkMode: undefined,
+    pageLoading: false,
+    projectConfig: Persistent.getLocal(PROJ_CFG_KEY),
+    beforeMiniInfo: {},
+  }),
+  getters: {
+    getPageLoading(): boolean {
+      return this.pageLoading;
+    },
+    getDarkMode(): 'light' | 'dark' | string {
+      return this.darkMode || localStorage.getItem(APP_DARK_MODE_KEY_) || darkMode;
+    },
+    getBeforeMiniInfo(): BeforeMiniState {
+      return this.beforeMiniInfo;
+    },
+    getProjectConfig(): ProjectConfig {
+      return this.projectConfig || ({} as ProjectConfig);
+    },
+    getHeaderSetting(): HeaderSetting {
+      return this.getProjectConfig.headerSetting;
+    },
+    getMenuSetting(): MenuSetting {
+      return this.getProjectConfig.menuSetting;
+    },
+    getTransitionSetting(): TransitionSetting {
+      return this.getProjectConfig.transitionSetting;
+    },
+    getMultiTabsSetting(): MultiTabsSetting {
+      return this.getProjectConfig.multiTabsSetting;
+    },
+    getSysConfigInfo(): SysConfigInfo {
+      return this.getProjectConfig.sysConfigInfo;
+    },
+  },
+  actions: {
+    setPageLoading(loading: boolean): void {
+      this.pageLoading = loading;
+    },
+    setDarkMode(mode: ThemeEnum): void {
+      this.darkMode = mode;
+      localStorage.setItem(APP_DARK_MODE_KEY_, mode);
+    },
+    setBeforeMiniInfo(state: BeforeMiniState): void {
+      this.beforeMiniInfo = state;
+    },
+    setProjectConfig(config: DeepPartial<ProjectConfig>): void {
+      this.projectConfig = deepMerge(this.projectConfig || {}, config as any);
+      Persistent.setLocal(PROJ_CFG_KEY, this.projectConfig);
+    },
+    async resetAllState() {
+      resetRouter();
+      Persistent.clearAll();
+    },
+    async setPageLoadingAction(loading: boolean): Promise<void> {
+      if (loading) {
+        clearTimeout(timeId);
+        // Prevent flicker
+        timeId = setTimeout(() => {
+          this.setPageLoading(loading);
+        }, 50);
+      } else {
+        this.setPageLoading(loading);
+        clearTimeout(timeId);
+      }
+    },
+  },
+});
+
+// Need to be used outside the setup（todo..）
+export function useAppStoreWithOut() {
+  return useAppStore(store);
 }
